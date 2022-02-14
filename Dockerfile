dc39d0a9c20197263f5cf10a30c207d2fed49c4b
rising-star92 <ryan.service.1001@gmail.com>
2022-02-14 21:35:59 +0200

    Outputs for UI (#276)

--- Dockerfile
+++ Dockerfile
@@ -14,10 +14,6 @@ RUN cd /tmp && curl -fsSL https://get.pulumi.com | sh -s -- --version ${PULUMI_V
     && echo "export PULUMI_SKIP_UPDATE_CHECK=false" >> /root/.zshrc
 ENV PATH="/root/.pulumi/bin:${PATH}"
 
-# Pulumi Plugins (Temporary, until we migrate away from v0.0.6)
-RUN ${HOME}/.pulumi/bin/pulumi plugin install resource databricks 0.0.6 --server https://github.com/ingenii-solutions/pulumi-databricks/releases/download/v0.0.6
-RUN ${HOME}/.pulumi/bin/pulumi plugin install resource databricks 0.0.8 --server https://github.com/ingenii-solutions/pulumi-databricks/releases/download/v0.0.8
-
 # Copy the platform source code to the container
 COPY src /platform/src
 
--- src/scripts/export_outputs_for_ui.py
+++ src/scripts/export_outputs_for_ui.py
@@ -4,9 +4,11 @@ table that will be read and processed by the Ingenii UI.
 """
 import argparse
 import json
+import subprocess
+import yaml
 from typing import Dict, Any
 
-from azure.data.tables import TableServiceClient
+from azure.data.tables import TableServiceClient, TableClient
 from azure.core.exceptions import ResourceNotFoundError
 
 parser = argparse.ArgumentParser(
@@ -22,22 +24,6 @@ parser.add_argument(
 )
 
 parser.add_argument(
-    "--connection-string",
-    type=str,
-    nargs="?",
-    help="The connection string of the Azure storage account.",
-    required=True,
-)
-
-parser.add_argument(
-    "--table-name",
-    type=str,
-    nargs="?",
-    help="The name of the table in the Azure storage account.",
-    required=True,
-)
-
-parser.add_argument(
     "--env-name",
     type=str,
     nargs="?",
@@ -45,82 +31,139 @@ parser.add_argument(
     required=True,
 )
 
+parser.add_argument(
+    "--ui-project-name",
+    type=str,
+    nargs="?",
+    help="The name of the platform environment. (dev, test, prod or shared)",
+    required=False,
+    default="iiui",
+)
+
+parser.add_argument(
+    "--ui-stack-name",
+    type=str,
+    nargs="?",
+    help="The name of the platform environment. (dev, test, prod or shared)",
+    required=False,
+    default="ingenii/test",
+)
+
 args = parser.parse_args()
 
 
-class OutputExporter:
-    def __init__(self, connection_string: str, table_name: str):
-        table_service = TableServiceClient.from_connection_string(connection_string)
-        self._client = table_service.get_table_client(table_name)
+def create_pulumi_project_file(project_name: str):
+    pulumi_project_file = {"name": project_name, "runtime": {"name": "python"}}
+    with open("Pulumi.yaml", "w") as outfile:
+        yaml.dump(pulumi_project_file, outfile, default_flow_style=False)
 
-    def _parse_outputs(self, file: str, env_name: str) -> Dict[Any, Any]:
-        with open(file, "r") as f:
-            outputs = json.loads(f.read())["root"]
 
-        parsed = {
-            "org_id": outputs["metadata"]["org_id"],
-            "project_id": outputs["metadata"]["project_id"],
-            "payload": {},
+def get_storage_connection_details(stack: str):
+    ui_outputs = json.loads(
+        subprocess.run(
+            args=["pulumi", "stack", "output", "--stack", stack, "--json"],
+            check=True,
+            capture_output=True,
+        ).stdout
+    )
+
+    connection_string = ";".join(
+        [
+            "DefaultEndpointsProtocol=https",
+            f"AccountName={ui_outputs['general_storage_name']}",
+            f"AccountKey={ui_outputs['client_services_urls_table_keys']['keys'][0]['value']}",
+            "EndpointSuffix=core.windows.net",
+        ]
+    )
+
+    return {
+        "table_name": ui_outputs["client_services_urls_table_name"],
+        "connection_string": connection_string,
+    }
+
+
+def get_table_client(conn_details: dict):
+    table_service = TableServiceClient.from_connection_string(
+        conn_details["connection_string"]
+    )
+    return table_service.get_table_client(conn_details["table_name"])
+
+
+def parse_platform_outputs(file: str, env_name: str) -> Dict[Any, Any]:
+    with open(file, "r") as f:
+        outputs = json.loads(f.read())["root"]
+
+    parsed = {
+        "org_id": outputs["metadata"]["org_id"],
+        "project_id": outputs["metadata"]["project_id"],
+        "payload": {},
+    }
+
+    payload = parsed["payload"]
+
+    if env_name in ["dev", "test", "prod"]:
+        databricks = outputs["analytics"]["databricks"]["workspaces"]
+        datafactory = outputs["analytics"]["datafactory"]["factories"]
+        datalake = outputs["storage"]["datalake"]
+        dbt = outputs["analytics"]["dbt"]["documentation"]
+
+        payload[env_name] = {
+            "databricks_eng_workspace_url": databricks.get("engineering", {}).get(
+                "url"
+            ),
+            "databricks_atc_workspace_url": databricks.get("analytics", {}).get("url"),
+            "datafactory_data_studio_url": datafactory.get("data", {}).get("url"),
+            "datalake_containers_view_url": datalake.get("containers_view_url"),
+            "dbt_docs_url": dbt.get("url"),
+        }
+    elif env_name == "shared":
+        payload[env_name] = {
+            "devops_project_url": outputs.get("devops", {})
+            .get("project", {})
+            .get("url")
         }
 
-        payload = parsed["payload"]
-
-        if env_name in ["dev", "test", "prod"]:
-            databricks = outputs["analytics"]["databricks"]["workspaces"]
-            datafactory = outputs["analytics"]["datafactory"]["factories"]
-            datalake = outputs["storage"]["datalake"]
-            dbt = outputs["analytics"]["dbt"]["documentation"]
-
-            payload[env_name] = {
-                "databricks_eng_workspace_url": databricks["engineering"].get("url"),
-                "databricks_atc_workspace_url": databricks["analytics"].get("url"),
-                "datafactory_data_studio_url": datafactory["data"].get("url"),
-                "datalake_containers_view_url": datalake.get("containers_view_url"),
-                "dbt_docs_url": dbt.get("url"),
-            }
-        elif env_name == "shared":
-            payload[env_name] = {
-                "devops_project_url": outputs["devops"]["project"].get("url")
-            }
-
-        return parsed
-
-    def save(self, outputs_file: str, env_name: str) -> None:
-        parsed = self._parse_outputs(outputs_file, env_name)
-
-        new_entity = {
-            "PartitionKey": parsed["org_id"],
-            "RowKey": parsed["project_id"],
-            "payload": parsed["payload"],
-        }
-
-        # If there is an existing record (row) in Azure Table storage,
-        # let's pull it in and merge our outputs to it.
-        try:
-            existing_entity = self._client.get_entity(
-                new_entity["PartitionKey"], new_entity["RowKey"]
-            )
-
-            existing_payload = json.loads(existing_entity["payload"])
-
-            # Merge the new payload (urls) with whatever is already existing on the Azure table storage.
-            # The new payload will overwrite existing data.
-            new_entity["payload"] = existing_payload | new_entity["payload"]
-        except ResourceNotFoundError:
-            # The table row has not been found, but that's ok.
-            # We'll upsert the payload which will result in the row being created.
-            pass
-        except KeyError:
-            # A KeyError can be generated if the "payload" column is empty or does not exist for that row.
-            # We can safely ignore the error and let the upsert function create a new row.
-            pass
-
-        new_entity["payload"] = json.dumps(new_entity["payload"])
-
-        # Write to Azure table storage
-        self._client.upsert_entity(new_entity)
+    return parsed
 
 
-# Main
-exporter = OutputExporter(args.connection_string, args.table_name)
-exporter.save(args.outputs_file, str(args.env_name).lower())
+def save_platform_outputs(parsed_outputs: dict[str, Any], client: TableClient) -> None:
+
+    new_entity = {
+        "PartitionKey": parsed_outputs["org_id"],
+        "RowKey": parsed_outputs["project_id"],
+        "payload": parsed_outputs["payload"],
+    }
+
+    # If there is an existing record (row) in Azure Table storage,
+    # let's pull it in and merge our outputs to it.
+    try:
+        existing_entity = client.get_entity(
+            new_entity["PartitionKey"], new_entity["RowKey"]
+        )
+
+        existing_payload = json.loads(existing_entity["payload"])
+
+        # Merge the new payload (urls) with whatever is already existing on the Azure table storage.
+        # The new payload will overwrite existing data.
+        new_entity["payload"] = existing_payload | new_entity["payload"]
+    except ResourceNotFoundError:
+        # The table row has not been found, but that's ok.
+        # We'll upsert the payload which will result in the row being created.
+        pass
+    except KeyError:
+        # A KeyError can be generated if the "payload" column is empty or does not exist for that row.
+        # We can safely ignore the error and let the upsert function create a new row.
+        pass
+
+    new_entity["payload"] = json.dumps(new_entity["payload"])
+
+    # Write to Azure table storage
+    client.upsert_entity(new_entity)
+
+
+if __name__ == "__main__":
+    create_pulumi_project_file(project_name=args.ui_project_name)
+    conn_details = get_storage_connection_details(stack=args.ui_stack_name)
+    table_client = get_table_client(conn_details)
+    outputs = parse_platform_outputs(args.outputs_file, str(args.env_name).lower())
+    save_platform_outputs(outputs, table_client)
